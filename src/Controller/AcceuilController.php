e9e136d9a054362d2b6e4b3733786e1e77c691cd
yasmine9295 <yasmine.elharim92@gmail.com>
2024-05-07 10:58:08 +0200

    ze

---
+++ src/Controller/AcceuilController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AcceuilController extends AbstractController
+{
+    /**
+     * @Route("/accueil", name="app_acceuil", methods={"GET "})
+     */
+    public function index(): Response
+    {
+        return $this->render('acceuil/index.html.twig', );
+    }
+
+}
+
+
+
+
+
+
